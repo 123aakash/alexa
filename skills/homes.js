1f602871cecdb93fb2ae61da0dedf1cafd098f4c
Aakash Sharma <aasharma@ex2india.com>
2018-11-27 12:28:48 +0530

    alexa test

---
+++ skills/homes.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const Alexa = require('ask-sdk');
+
+
+router.post('/',function(req, res){
+    var req = req.body;
+});
+
+modules.export = router;
