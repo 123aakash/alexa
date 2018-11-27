f2df32d6abb65158bffed3a6a017e848055b78ba
Aakash Sharma <aasharma@ex2india.com>
2018-11-27 19:38:38 +0530

    alexa test

--- skills/routes.js
+++ skills/routes.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express();
+var router = express.Router();
 var handler = require('./handlers/handler').handler;
 
 router.post('/', async function (req, res) {
@@ -10,7 +10,7 @@ router.post('/', async function (req, res) {
 
     const response = await handler(data, data.context);
     console.log("aakash:", typeof data, JSON.stringify(response));
-    return res.json(response);
+    return res.send(response);
 });
 
 module.exports = router;
