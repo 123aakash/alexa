d5d28378b693e2315441c48e00b7e59a49b0927e
Aakash Sharma <aasharma@ex2india.com>
2018-11-27 15:14:58 +0530

    alexa test

---
+++ skills/handlers/handler.js
@@ -0,0 +1,47 @@
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        //any cleanup logic goes here
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+
+exports.handler = async function (event, context) {
+    console.log(`REQUEST++++${JSON.stringify(event)}`);
+
+    // HelloWorldIntentHandler,
+    //       HelpIntentHandler,
+    //       CancelAndStopIntentHandler,
+    if (!skill) {
+      skill = Alexa.SkillBuilders.custom()
+        .addRequestHandlers(
+          LaunchRequestHandler,
+          SessionEndedRequestHandler,
+        )
+        .addErrorHandlers(ErrorHandler)
+        .create();
+    }
+  
+    const response = await skill.invoke(event, context);
+    console.log(`RESPONSE++++${JSON.stringify(response)}`);
+  
+    return response;
+  };
---
+++ skills/routes.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+const Alexa = require('ask-sdk');
+var handler = require('./handlers/handler');
+
+
+
+var skill;
+router.post('/', async function (req, res) {
+    var data = req.body;
+    console.log(req);
+    let context = data.context;
+    let event = "event";
+
+    let response = handler(data,data.context); 
+    return res.json(response);
+});
+
+module.exports = router;
