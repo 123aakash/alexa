a42089260064e824c96c20aab5c4e1b366513995
Aakash Sharma <aasharma@ex2india.com>
2018-11-27 11:52:35 +0530

    alexa test

--- index.js
+++ index.js
@@ -7,23 +7,8 @@ app.get('/',(req,res)=>{
 });
 const port = process.env.PORT || 3000;
 
+app.listen(process.env.PORT || 1337, () => console.log('alexa-webhook is listening'));
 
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
-
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention.
-//=========================================================================================================================================
-
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
-//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
 const APP_ID = 'amzn1.ask.skill.1294b7e3-a0a0-432c-a61a-83d70e04678a';
 
 const SKILL_NAME = 'Space Facts';
@@ -32,137 +17,126 @@ const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... 
 const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
 
-//=========================================================================================================================================
-//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
-//=========================================================================================================================================
-const data = [
-    'A year on Mercury is just 88 days long.',
-    'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-    'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-    'On Mars, the Sun appears about half the size as it does on Earth.',
-    'Earth is the only planet not named after a god.',
-    'Jupiter has the shortest day of all the planets.',
-    'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-    'The Sun contains 99.86% of the mass in the Solar System.',
-    'The Sun is an almost perfect sphere.',
-    'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-    'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-    'The temperature inside the Sun can reach 15 million degrees Celsius.',
-    'The Moon is moving approximately 3.8 cm away from our planet every year.',
-];
-
-//=========================================================================================================================================
-//Editing anything below this line might break your skill.
-//=========================================================================================================================================
-
-const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-      const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+app.post('/bot-webhook', (req, res) => {
+    let body = req.body;
+    console.log(body);
   
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .withSimpleCard('Hello World', speechText)
-        .getResponse();
-    }
-  };
-
-  const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-      const speechText = 'Hello World!';
+    var sessionId = body.sessionId;
+    var requestType = body.requestType;
+    var botResponse = body.msg;
+    var responseObj = {};
   
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .withSimpleCard('Hello World', speechText)
-        .getResponse();
-    }
-  };
-
-  const HelpIntentHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput) {
-      const speechText = 'You can say hello to me!';
+    switch (requestType) {
   
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .reprompt(speechText)
-        .withSimpleCard('Hello World', speechText)
-        .getResponse();
-    }
-  };
-
-  const CancelAndStopIntentHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-        && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-          || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput) {
-      const speechText = 'Goodbye!';
+      case "LaunchRequest":
+        responseObj = {
+          sessionId: sessionId,
+          alexaResponse: {
+            version: "string",
+            response: {
+              outputSpeech: {
+                type: 'PlainText',
+                text: "Hi, I am happy that you are here. maverickjoy welcomes you."
+              },
+              reprompt: {
+                outputSpeech: {
+                  type: "PlainText",
+                  text: "Would you like me to repeat"
+                }
+              },
+              shouldEndSession: false
+            }
+          }
+        }
+        break;
   
-      return handlerInput.responseBuilder
-        .speak(speechText)
-        .withSimpleCard('Hello World', speechText)
-        .getResponse();
-    }
-  };
-
-
-  const SessionEndedRequestHandler = {
-    canHandle(handlerInput) {
-      return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-    },
-    handle(handlerInput) {
-      //any cleanup logic goes here
-      return handlerInput.responseBuilder.getResponse();
-    }
-  };
-
-  const ErrorHandler = {
-    canHandle() {
-      return true;
-    },
-    handle(handlerInput, error) {
-      console.log(`Error handled: ${error.message}`);
+      case "IntentRequest":
+        responseObj = {
+          sessionId: sessionId,
+          alexaResponse: {
+            version: "string",
+            response: {
+              outputSpeech: {
+                type: 'PlainText',
+                text: botResponse
+              },
+              reprompt: {
+                outputSpeech: {
+                  type: "PlainText",
+                  text: "Would you like me to repeat"
+                }
+              },
+              shouldEndSession: false
+            }
+          }
+        }
+        break;
   
-      return handlerInput.responseBuilder
-        .speak('Sorry, I can\'t understand the command. Please say again.')
-        .reprompt('Sorry, I can\'t understand the command. Please say again.')
-        .getResponse();
-    },
-  };
-
-
-
-let skill;
-
-exports.handler = async function (event, context) {
-  console.log(`REQUEST++++${JSON.stringify(event)}`);
-  if (!skill) {
-    skill = Alexa.SkillBuilders.custom()
-      .addRequestHandlers(
-        LaunchRequestHandler,
-        HelloWorldIntentHandler,
-        HelpIntentHandler,
-        CancelAndStopIntentHandler,
-        SessionEndedRequestHandler,
-      )
-      .addErrorHandlers(ErrorHandler)
-      .create();
-  }
-
-  const response = await skill.invoke(event, context);
-  console.log(`RESPONSE++++${JSON.stringify(response)}`);
-
-  return response;
-};
+      case "StopRequest":
+        responseObj = {
+          sessionId: sessionId,
+          alexaResponse: {
+            version: "string",
+            response: {
+              outputSpeech: {
+                type: 'PlainText',
+                text: "Thank You for Talking To maverickjoy. Bye"
+              },
+              reprompt: {
+                outputSpeech: {
+                  type: "PlainText",
+                  text: "Would you like me to repeat"
+                }
+              },
+              shouldEndSession: true
+            }
+          }
+        }
+        break;
+  
+      default:
+        responseObj = {
+          sessionId: sessionId,
+          alexaResponse: {
+            version: "string",
+            response: {
+              outputSpeech: {
+                type: 'PlainText',
+                text: "Unknown-Type"
+              },
+              reprompt: {
+                outputSpeech: {
+                  type: "PlainText",
+                  text: "Would you like me to repeat"
+                }
+              },
+              shouldEndSession: false
+            }
+          }
+        }
+  
+    }
+  
+    return responseAlexaWebhook(responseObj);
+  
+  });
+  
+  function responseAlexaWebhook(responseObj) {
+  
+    request({
+      uri: config.ALEXA_WEBHOOK_URL + '/alexa-webhook-response',
+      method: 'POST',
+      json: responseObj,
+      headers: {
+        'Content-type': 'application/json',
+      },
+    }, function(error, response, body) {
+      if (error) {
+        console.log('Error:', error);
+        return;
+      } else {
+        console.log('Response sent to Alexa-Webhook');
+        return;
+      }
+    });
+  
