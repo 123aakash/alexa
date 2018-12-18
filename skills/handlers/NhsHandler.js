422c9ed2564e5e0515a2a49ca4e1919f52c5c0d7
Aakash Sharma <aasharma@ex2india.com>
2018-12-18 10:19:16 +0530

    added list response

--- skills/handlers/NhsHandler.js
+++ skills/handlers/NhsHandler.js
@@ -2,6 +2,7 @@ const SearchModel = require('../../api/models/SearchModel');
 const city = require('../../api/city');
 var ApiRequest = require('../../api/ApiRequest');
 
+const Alexa = require('ask-sdk');
 
 
 let getHomes = (cityObject) => {
@@ -17,7 +18,7 @@ let createResponse = (data) => {
     console.log('final result data:', data);
     var sm = new SearchModel(JSON.parse(data).Result);
 
-    return a.responseBuilder
+    return handlerInput.responseBuilder
         .speak('Heres some data:' + JSON.stringify(sm.data[0]['HomeId']))
         .withSimpleCard("NONE")
         .getResponse();
@@ -31,9 +32,44 @@ const NhsHandler = {
     handle(handlerInput) {
         const speechText = 'Searching for homes';
         const intentSlots = handlerInput.requestEnvelope.request.intent.slots;
-        a = handlerInput;
+        let responseBuilder = handlerInput.responseBuilder;
+
         return city.getCityDetails(intentSlots.city.value)
-            .then(getHomes).then(createResponse);
+            .then(getHomes)
+            .then((data) => {
+                var searchModel = new SearchModel(JSON.parse(data).Result);
+                console.log('final result data:', searchModel);
+
+                let homeList = [];
+                searchModel.data.forEach((home) => {
+                    const homeImage = new Alexa.ImageHelper().withDescription(`Home Image`);
+                    homeImage.addImageInstance(home.Thumb1);
+
+                    homeList.push({
+                        token: home.HomeId,
+                        textContent: new Alexa.PlainTextContentHelper()
+                            .withPrimaryText(home.PlanName)
+                            .withSecondaryText(`Abbreviation: ${home.HomeId}`)
+                            .withTertiaryText(`Capital: ${home.CommName}`)
+                            .getTextContent(),
+                        image: homeImage.getImage()
+
+                    });
+                });
+
+                responseBuilder.addRenderTemplateDirective({
+                    type: `ListTemplate1`,
+                    token: 'listToken',
+                    backButton: 'hidden',
+                    title: `I list things like this:`,
+                    listItems: homeList,
+                });
+
+                return responseBuilder
+                    .speak('Heres some data:' + JSON.stringify(searchModel.data[0]['HomeId']))
+                    .withStandardCard('')
+                    .getResponse();
+            });
     }
 }
 
